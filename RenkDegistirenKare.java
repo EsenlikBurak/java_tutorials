648d48840cf7fd3a9023add0e87ad4de0b7955b9
Burak Esenlik <130107972+EsenlikBurak@users.noreply.github.com>
2023-04-20 13:38:53 +0300

    renk_degistiren_kare

---
+++ RenkDegistirenKare.java
@@ -0,0 +1,21 @@
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+public class RenkDegistirenKare extends GraphicsProgram {
+
+	/** Programi calistirir */
+	public void run() {
+		// kodunuzu buraya yaziniz
+	}
+
+	/** Karenin piksel cinsinden bir boyutu */
+	private static final double KARE_BOYUTU = 100;
+
+	/** Milisaniye cinsinden bekleme suresi */
+	private static final double BEKLEME_SURESI = 1000;
+
+	/* Instance variables */
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+}
