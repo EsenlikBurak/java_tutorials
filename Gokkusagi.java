5ebbb60d52cfb32fcfee10240862ac3ffde98e57
Burak Esenlik <130107972+EsenlikBurak@users.noreply.github.com>
2023-04-20 13:46:42 +0300

    Basic Java tutorials

---
+++ Gokkusagi.java
@@ -0,0 +1,19 @@
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class Gokkusagi extends GraphicsProgram {
+
+	public void run() { 
+		GRect arkaPlan = new GRect(0,0,getWidth(), getHeight());
+		arkaPlan.setColor(Color.CYAN);
+		arkaPlan.setFillColor(Color.CYAN);
+		arkaPlan.setFilled(true);
+		add(arkaPlan);
+
+		for (int i = 0; i < 7; i++){
+			// kodunuzu buraya yaziniz
+		}
+	}
+}
